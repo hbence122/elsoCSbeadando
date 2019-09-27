17c4465b6dabdba5f322b16101d5d314724e12f8
Horváth Bence <hbence122@gmail.com>
2019-09-27 15:28:34 +0200

    Add project files.

---
+++ CSbeadando/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSbeadando
+{
+    class Program
+    {
+        static List<int> szamok = new List<int>();
+
+        static void Main(string[] args)
+        {
+            int bekert;
+            Console.WriteLine("Adjon meg egy számot: ");
+        }
+    }
+}
